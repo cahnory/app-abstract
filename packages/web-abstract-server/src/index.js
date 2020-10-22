10a05085563e2f82fad618678d34b4bd76c71768
cahnory <cahnory@gmail.com>
2020-10-22 20:32:45 +0200

    setup eslint in server package

--- packages/web-abstract-server/src/index.js
+++ packages/web-abstract-server/src/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
-import { PORT, IS_DEV } from './constants';
+import { IS_DEV, PORT } from './constants';
 
 express()
   /* Request logger */
   .use(morgan(IS_DEV ? 'dev' : 'combined'))
   .listen(PORT, (err) => {
     if (err) {
+      // eslint-disable-next-line no-console
       console.log(`💥  server failed to listen on port ${PORT}: ${err.code}`);
     } else {
+      // eslint-disable-next-line no-console
       console.log(`🚀  Server listening on port ${PORT}`);
     }
   });
