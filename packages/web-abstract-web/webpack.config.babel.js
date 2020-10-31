fb79e6b50a5bedf10c608cde01183dd6613677ee
cahnory <cahnory@gmail.com>
2020-10-31 17:59:02 +0100

    simplify webpack conf

--- packages/web-abstract-web/webpack.config.babel.js
+++ packages/web-abstract-web/webpack.config.babel.js
@@ -1,80 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
-import WebpackAssetPlugin from 'assets-webpack-plugin';
+import AssetPlugin from 'assets-webpack-plugin';
 
 const OUTPUT_PATH = path.resolve(__dirname, 'lib');
 const PUBLIC_PATH = path.resolve(OUTPUT_PATH, 'public');
 const MANIFEST_OUTPUT = 'manifest.json';
 const PUBLIC_ROUTE = '/assets';
 
-export default (_, { watch, mode = watch ? 'development' : 'production' }) => {
-  const server = makeConfig({
-    name: 'server',
-    target: 'node',
-    path: OUTPUT_PATH,
-    entry: {
-      server: ['./src/server'],
-    },
-    libraryTarget: 'umd',
-    emitFile: false,
-    stats: 'minimal',
-  });
+export default (_, { watch }) => [
+  makeConfig({ isServer: false, isDevelopment: watch }),
+  makeConfig({ isServer: true, isDevelopment: watch }),
+];
 
-  const client = makeConfig({
-    name: 'client',
-    path: PUBLIC_PATH,
-    entry: {
-      client: './src/client',
-      vendors: ['react', 'react-dom'],
-    },
-    filename: '[name].[contenthash].js',
-    plugins: [
-      new WebpackAssetPlugin({
-        filename: MANIFEST_OUTPUT,
-        path: OUTPUT_PATH,
-        prettyPrint: true,
-        fullPath: true,
-        processOutput: (assets) => {
-          return JSON.stringify(
-            {
-              publicPath: PUBLIC_ROUTE,
-              bundlePath: PUBLIC_PATH,
-              assets,
-            },
-            null,
-            2,
-          );
-        },
-      }),
-    ],
-  });
-
-  return [client, server];
-};
-
-const makeConfig = ({
-  mode,
-  path: outputPath,
-  filename,
-  name,
-  target,
-  entry,
-  libraryTarget,
-  stats,
-  emitFile,
-  plugins,
-}) => ({
-  mode,
-  name,
-  target,
-  entry,
-  plugins,
-  stats,
+const makeConfig = ({ isServer = false, isDevelopment = false }) => ({
+  name: isServer ? 'server' : 'client',
+  mode: isDevelopment ? 'development' : 'production',
+  target: isServer ? 'node' : 'web',
+  stats: isServer ? 'minimal' : 'normal',
+  entry: isServer
+    ? {
+        server: ['./src/server'],
+      }
+    : {
+        client: './src/client',
+        vendors: ['react', 'react-dom'],
+      },
   output: {
     publicPath: `${PUBLIC_ROUTE}/`,
-    path: outputPath,
-    libraryTarget,
-    filename,
+    path: isServer ? OUTPUT_PATH : PUBLIC_PATH,
+    libraryTarget: 'umd',
+    filename: isServer ? '[name].js' : '[name].[contenthash].js',
   },
   module: {
     rules: [
@@ -94,11 +49,31 @@ const makeConfig = ({
             loader: 'file-loader',
             options: {
               name: '[contenthash].[ext]',
-              emitFile,
+              emitFile: !isServer,
             },
           },
         ],
       },
     ],
   },
+  plugins: [
+    !isServer &&
+      new AssetPlugin({
+        filename: MANIFEST_OUTPUT,
+        path: OUTPUT_PATH,
+        prettyPrint: true,
+        fullPath: true,
+        processOutput: (assets) => {
+          return JSON.stringify(
+            {
+              publicPath: PUBLIC_ROUTE,
+              bundlePath: PUBLIC_PATH,
+              assets,
+            },
+            null,
+            2,
+          );
+        },
+      }),
+  ].filter(Boolean),
 });
