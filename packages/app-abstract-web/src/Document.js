1774a8754adfecb34acec978867c728abebe7075
cahnory <cahnory@gmail.com>
2020-11-03 07:55:31 +0100

    setup eslint in web package

--- packages/app-abstract-web/src/Document.js
+++ packages/app-abstract-web/src/Document.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Document = ({ body, scriptElements, linkElements, styleElements }) => (
-  <html>
+const Document = ({
+  body,
+  lang,
+  scriptElements,
+  linkElements,
+  styleElements,
+}) => (
+  <html lang={lang}>
     <head>
       <title>Hello world</title>
       <meta charSet="utf-8" />
@@ -9,10 +16,19 @@ const Document = ({ body, scriptElements, linkElements, styleElements }) => (
       {styleElements}
     </head>
     <body>
+      {/* eslint-disable-next-line react/no-danger */}
       <div id="root" dangerouslySetInnerHTML={{ __html: body }} />
       {scriptElements}
     </body>
   </html>
 );
+
+Document.propTypes = {
+  body: PropTypes.string.isRequired,
+  lang: PropTypes.string.isRequired,
+  scriptElements: PropTypes.node.isRequired,
+  linkElements: PropTypes.node.isRequired,
+  styleElements: PropTypes.node.isRequired,
+};
 
 export default Document;
